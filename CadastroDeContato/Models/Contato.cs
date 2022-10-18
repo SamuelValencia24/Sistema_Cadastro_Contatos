1c8c345e9fbfa2a56c4defbe7e69b2f98efb1f93
Samuel Valencia <sv807227@gmail.com>
2022-10-17 21:35:41 -0300

    Sistema De Cadastro De Contatos/Versão 1.0
    Sistema para criação e armazenamento de informações para Cadastro de Contatos. Primeira versão, sendo a versão 1.0,

---
+++ CadastroDeContato/Models/Contato.cs
@@ -0,0 +1,10 @@
+﻿namespace CadastroDeContato.Models
+{
+    public class Contato
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}
---
+++ CadastroDeContato/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CadastroDeContato.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
